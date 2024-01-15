105e62f6c4210aa48174ba8711d5e99128e9b065
BorjaOteroFerreira <115078003+BorjaOteroFerreira@users.noreply.github.com>
2024-01-15 13:03:52 +0100

    .

--- app.py
+++ app.py
@@ -5,17 +5,10 @@ from flask_cors import CORS
 
 
 app = Flask(__name__)
-
 socketio = SocketIO(app, cors_allowed_origins="*")
 # Instanciar el asistente de Llama
 model_path = "./models/llama-2-7b-chat.Q8_0.gguf"
 llama_assistant = LlamaAssistant(model_path=model_path)
-
-
-@app.route('/static/<path:filename>')
-def static_file(filename):
-    return send_from_directory(app.static_folder, filename, cache_timeout=0)
-
 
 @app.after_request
 def add_header(response):
@@ -23,7 +16,11 @@ def add_header(response):
     response.headers['Pragma'] = 'no-cache'
     response.headers['Expires'] = '-1'
     return response
-    
+
+@app.route('/static/<path:filename>')
+def static_file(filename):
+    return send_from_directory(app.static_folder, filename, cache_timeout=0)
+
 @app.route("/")
 def index():
     return render_template("index_socketio.html")
@@ -37,13 +34,11 @@ def clear_context():
 @app.route("/ask", methods=["POST"])
 def ask():
     user_input = request.form["user_input"]
-
     # Añadir input del usuario al asistente de Llama
     llama_assistant.add_user_input(user_input)
     print(user_input)
     # Obtener la respuesta del asistente
     response = llama_assistant.get_assistant_response()
-    
     # Enviar la respuesta a través del socket
     socketio.emit("response", {"role": "assistant", "content": response})
 
@@ -51,6 +46,5 @@ def ask():
 
 if __name__ == "__main__":
     CORS(app)
-    # Iniciar la aplicación Flask y SocketIO
     if __name__ == '__main__':
         socketio.run(app, host='0.0.0.0', port=8080, debug=True)
