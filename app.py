a51a3e75d263dce1f0d8041e469de44b1f18ade9
BorjaOteroFerreira <115078003+BorjaOteroFerreira@users.noreply.github.com>
2024-02-15 00:42:17 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,12 @@
 @Author: Borja Otero Ferreira
 '''
 from IASuite import IASuiteApi
+import webbrowser
 
 
 if __name__ == '__main__':
     chat_app = IASuiteApi()
-    chat_app.socketio.run(chat_app.app, host='0.0.0.0', port=8080, debug=False)
+    chat_app.socketio.run(webbrowser.open('http://127.0.0.1:8080',2,False) and chat_app.app, host='0.0.0.0', port=8080, debug=False)
+
+   
 
