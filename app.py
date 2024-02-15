6b35f0a61934e8b9b35b4576cfe4603fd18ec3f6
BorjaOteroFerreira <115078003+BorjaOteroFerreira@users.noreply.github.com>
2024-02-15 17:31:01 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,11 +2,13 @@
 @Author: Borja Otero Ferreira
 '''
 from IASuite import IASuiteApi
-import webbrowser
+from flask_cors import CORS 
+
 
 
 if __name__ == '__main__':
     chat_app = IASuiteApi()
+    CORS(chat_app.app, origins=["http://127.0.0.1:8080", "http://[::1]:8080"])
     chat_app.socketio.run(chat_app.app, host='0.0.0.0', port=8080, debug=False)
 
    
