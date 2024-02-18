0f3136c107f86a868b82ecbd09c7830dc005465e
BorjaOteroFerreira <115078003+BorjaOteroFerreira@users.noreply.github.com>
2024-02-18 07:37:06 +0100

    .

--- static/js/chat.js
+++ static/js/chat.js
@@ -53,14 +53,18 @@ class Chat {
         const tableRegex = /(?:\|.*(?:\|).*)+\|/gs;
         let htmlResponse = this.response.replace(tableRegex, (table) => {
             const rows = table.trim().split('\n').map(row => row.trim().split('|').filter(cell => cell.trim() !== ''));
+        
+            // Filtrar las filas que contienen guiones
+            const filteredRows = rows.filter(row => !row.some(cell => cell.includes('----')));
+        
             let htmlTable = '<table>';
-            for (let i = 0; i < rows.length; i++) {
+            for (let i = 0; i < filteredRows.length; i++) {
                 htmlTable += '<tr>';
-                for (let j = 0; j < rows[i].length; j++) {
+                for (let j = 0; j < filteredRows[i].length; j++) {
                     if (i === 0) {
-                        htmlTable += `<th>${rows[i][j]}</th>`;
+                        htmlTable += `<th>${filteredRows[i][j]}</th>`;
                     } else {
-                        htmlTable += `<td>${rows[i][j]}</td>`;
+                        htmlTable += `<td>${filteredRows[i][j]}</td>`;
                     }
                 }
                 htmlTable += '</tr>';
@@ -79,8 +83,6 @@ class Chat {
         divAssistant.find('pre code').each(function(i, block) {
             Prism.highlightElement(block);
         });
-    
-        this.scrollToBottom();
     }
 
     clearChat() {
