0eb67a36446e12fe9c529c6f1d71c77197f44962
BorjaOteroFerreira <115078003+BorjaOteroFerreira@users.noreply.github.com>
2025-06-22 23:00:31 +0200

    .

--- Frontend-React/src/index.js
+++ Frontend-React/src/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import './fonts.css';
+import './safari-mobile-fix.css';
 import './fontLoader';
 import App from './App'; // Usando la versión completa con el nuevo diseño
 
